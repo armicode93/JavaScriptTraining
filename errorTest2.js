47aae1e0072aeeb883d1e13bc180daf5a455bdd5
armicode93 <naserr93@hotmail.com>
2024-09-03 15:19:20 +0200

    Test Numbers

--- errorTest2.js
+++ errorTest2.js
@@ -1,7 +1,10 @@
 function afficher2(){
     var lastName = document.getElementById("lastName").value;
     var firstName = document.getElementById("firstName").value;
+    var age = document.getElementById("age").value;
     var error = document.getElementById("error");
+
+    var ageNumb = Number(age); 
 
     // Reset error message
     error.innerHTML = "";
@@ -10,12 +13,18 @@ function afficher2(){
         error.innerHTML = "Write your lastname";
         return;
     }
-
     if (firstName.length < 1) {
         error.innerHTML = "Write your firstname";
         return;
     }
+    //age.trim() === "" è una condizione che verifica se il campo age è vuoto o contiene solo spazi bianchi.
+    if(age < 1 || isNaN(ageNumb))
+    {
+        error.innerHTML = "Put a number";
+        
+        return;
+    }
     error.innerHTML = "";
     
-    alert("Welcome " + lastName + " " + firstName);
+    alert("Welcome " + lastName + " " + firstName+" "+age);
 }
